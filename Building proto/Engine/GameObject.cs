d3501c5129df1f8d8823a97ee2f74cc9c362a942
Tom <60851185+TJJonker@users.noreply.github.com>
2021-03-30 13:33:25 +0200

    Initial Commit + Engine: GameObject

---
+++ Building proto/Engine/GameObject.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine
+{
+    internal abstract class GameObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Direction { get; set; }
+
+        public virtual void Update(GameTime gameTime) { }
+        //public virtual void HandleInput(InputHelper inputHelper) { }
+        public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime) { }
+    }
+}
---
+++ Building proto/Game1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Building_proto
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
